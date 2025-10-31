8105138cc1f9f99085cd6a961c3997c7f6be7a3d
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2025-11-01 00:33:00 +0530

    Add basePath for custom domain with path routing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: '/powerpoint-generator',
+  assetPrefix: '/powerpoint-generator',
+}
 
 module.exports = nextConfig
